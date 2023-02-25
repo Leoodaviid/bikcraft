065de01b9df9999de84055771007dcd6646488d6
Leoodaviid <david_ax7@hotmail.com>
2023-02-25 15:51:18 -0300

    chamando rotas das pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
-
 import MainPage from './pages/Main'
+import Home from './pages/Home/Home'
+import Bicicletas from './pages/Bicicletas/Bicicletas'
+import Seguros from './pages/Seguros/Seguros'
+import Contato from './pages/Contato/Contato'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />}>
+        <Route path='/' element={<Home />} />
+        <Route path='bicicletas' element={<Bicicletas />} />
+        <Route path='seguros' element={<Seguros />} />
+        <Route path='contato' element={<Contato />} />
+      </Route>
     </Routes>
   )
 }
