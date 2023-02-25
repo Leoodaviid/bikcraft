7c56d8df4bda0b4d356dfb725a5b68c7f13ac119
Leoodaviid <david_ax7@hotmail.com>
2023-02-25 19:33:36 -0300

    estilos globais da  aplicacao

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,13 +4,33 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0px;
     font-family: Arial, Helvetica, sans-serif;
+    padding-bottom: 600px;
+  }
+  ::-webkit-scrollbar{
+  width: 10px;
+}
+::-webkit-scrollbar-track{
+  background: ${({ theme }) => theme.colors.white};
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb{
+  background: ${({ theme }) => theme.colors.yellow};
+  border-radius: 10px;
+}
+  h1, ul, p{
+    margin: 0px;
   }
   ul{
-    margin: 0px;
     padding: 0px;
     list-style: none;
   }
   a{
     text-decoration: none;
+    color: inherit;
   }
+  img{
+    max-width: 100%;
+    display: block;
+  }
+
 `
