7506e164a553063da3e0640c3501535f7deae148
Leoodaviid <david_ax7@hotmail.com>
2023-02-28 14:12:18 -0300

    adicionando classname opcional + estilos

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -2,11 +2,16 @@ import { ReactNode } from 'react'
 import { Button } from './styles'
 
 interface ButtonLinkProps {
+  className?: string
   to: string
   children: ReactNode
 }
-const LinkButton = ({ to, children }: ButtonLinkProps) => {
-  return <Button to={to}>{children}</Button>
+const LinkButton = ({ className, to, children }: ButtonLinkProps) => {
+  return (
+    <Button className={className} to={to}>
+      {children}
+    </Button>
+  )
 }
 
 export default LinkButton
--- src/components/LinkButton/styles.ts
+++ src/components/LinkButton/styles.ts
@@ -1,19 +1,33 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
 
-export const Button = styled(Link)`
+interface ButtonProps {
+  className?: string
+}
+export const Button = styled(Link)<ButtonProps>`
   display: inline-block;
   padding: 1rem 2rem;
-  background: linear-gradient(${({ theme }) => theme.colors.yellow}, #f2a50c);
+  ${({ className }) =>
+    className === 'secondary'
+      ? css`
+          background: ${({ theme }) => theme.colors.gray700};
+          color: ${({ theme }) => theme.colors.white};
+          &:hover {
+            background: ${({ theme }) => theme.colors.gray600};
+          }
+        `
+      : css`
+          background: linear-gradient(${({ theme }) => theme.colors.yellow}, #f2a50c);
+          color: ${({ theme }) => theme.colors.secondary};
+          &:hover {
+            background: linear-gradient(#ffb60d, #e59317);
+          }
+        `}
   border-radius: 0.25rem;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
   text-transform: uppercase;
-  color: ${({ theme }) => theme.colors.secondary};
   font: 600 ${({ theme }) => theme.fontSize.lg} / 1.35 ${({ theme }) => theme.fontFamily.heading};
-
-  &:hover {
-    background: linear-gradient(#ffb60d, #e59317);
-  }
+  max-width: max-content;
 
   @media (max-width: 600px) {
     padding: 0.75rem 1rem;
