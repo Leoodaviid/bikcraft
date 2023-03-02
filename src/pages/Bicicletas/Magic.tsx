fb2a146c0378a780bb934ef9562dcf418d03ae19
Leoodaviid <david_ax7@hotmail.com>
2023-03-02 16:29:19 -0300

    page bik magic

--- src/pages/Bicicletas/Magic.tsx
+++ src/pages/Bicicletas/Magic.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Article } from '../Home/styles'
+import { Bik } from '../../components/Bik/Bik'
+import { Head } from '../../components/Head/Head'
+import { UseTitle } from '../../components/Title/useTitle'
+import { BikSubTitle } from '../../components/BikTitle/BikSubTitle'
+import { BikListHome } from '../../components/BikList/BikListHome'
+import { SafeBanner } from '../../components/Safe/SafeBanner'
 
 const Magic = () => {
-  return <div>bike magick</div>
+  return (
+    <>
+      <Head title='Magic' />
+      <UseTitle />
+      <Bik BikNumber={1} name={'Magic Might'} km={45} />
+      <Article>
+        <BikSubTitle className='container'>Escolha a sua</BikSubTitle>
+        <BikListHome />
+      </Article>
+      <SafeBanner />
+    </>
+  )
 }
 
 export default Magic
