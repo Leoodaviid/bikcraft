722c82afc77edb4a5e59accc4d50123151469678
Leoodaviid <david_ax7@hotmail.com>
2023-03-02 00:16:54 -0300

    retirando arrays e incluindo em helper

--- src/components/BikList/BikList.tsx
+++ src/components/BikList/BikList.tsx
@@ -1,56 +1,11 @@
 import { Container } from './styles'
-import BikMagic from '../../assets/img/magic.jpg'
-import BikNimbus from '../../assets/img/nimbus.jpg'
-import BikNebula from '../../assets/img/nebula.jpg'
+import { ReactComponent as Seta } from '../../assets/icons/seta.svg'
+import { Biks } from '../Helper/BiksList'
 import InconEletrica from '../../assets/icons/eletrica.svg'
 import InconCarbono from '../../assets/icons/carbono.svg'
 import InconVelocidade from '../../assets/icons/velocidade.svg'
 import InconRastreador from '../../assets/icons/rastreador.svg'
-import { ReactComponent as Seta } from '../../assets/icons/seta.svg'
 import LinkButton from '../LinkButton/LinkButton'
-
-interface BikListProps {
-  id: number
-  img: string
-  price: string
-  name: string
-  description: string
-  km: number
-  to: string
-}
-
-const Biks = [
-  {
-    id: 1,
-    img: BikNimbus,
-    price: 'R$ 4999',
-    name: 'Nimbus Stark',
-    description:
-      'A Nimbus Stark é a melhor Bikcraft já criada pela nossa equipe. Ela vem equipada com os melhores acessórios, o que garante maior velocidade.',
-    km: 50,
-    to: '/bicicletas/nimbus',
-  },
-  {
-    id: 2,
-    img: BikMagic,
-    price: 'R$ 2499',
-    name: 'Magic Might',
-    description:
-      'A Magic Might é a melhor Bikcraft já criada pela nossa equipe. Ela vem equipada com os melhores acessórios, o que garante maior velocidade.',
-    km: 45,
-    to: '/bicicletas/magic',
-  },
-  {
-    id: 3,
-    img: BikNebula,
-    price: 'R$ 3999',
-    name: 'Nebula Cosmic',
-    description:
-      'A Nebula Cosmic é a melhor Bikcraft já criada pela nossa equipe. Ela vem equipada com os melhores acessórios, o que garante maior velocidade.',
-    km: 50,
-    to: '/bicicletas/nebula',
-  },
-] as BikListProps[]
 
 const BikList = () => {
   return (
