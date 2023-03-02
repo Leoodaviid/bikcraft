233322b88f57bd13bda55cca099b267ce6957a19
Leoodaviid <david_ax7@hotmail.com>
2023-03-02 16:29:36 -0300

    page bik Nebula

---
+++ src/pages/Bicicletas/Nebula.tsx
@@ -0,0 +1,24 @@
+import { Bik } from '../../components/Bik/Bik'
+import { BikListHome } from '../../components/BikList/BikListHome'
+import { BikSubTitle } from '../../components/BikTitle/BikSubTitle'
+import { Head } from '../../components/Head/Head'
+import { SafeBanner } from '../../components/Safe/SafeBanner'
+import { UseTitle } from '../../components/Title/useTitle'
+import { Article } from '../Home/styles'
+
+const Nebula = () => {
+  return (
+    <>
+      <Head title='Nebula' />
+      <UseTitle />
+      <Bik BikNumber={2} name={'Nebula Cosmic'} km={40} />
+      <Article>
+        <BikSubTitle className='container'>Escolha a sua</BikSubTitle>
+        <BikListHome />
+      </Article>
+      <SafeBanner />
+    </>
+  )
+}
+
+export default Nebula
