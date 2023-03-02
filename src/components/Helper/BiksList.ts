edfcf8de1270aaeae064b72bb3514d2fcd85932e
Leoodaviid <david_ax7@hotmail.com>
2023-03-02 14:29:19 -0300

    incluindo foto bik - home

--- src/components/Helper/BiksList.ts
+++ src/components/Helper/BiksList.ts
@@ -1,10 +1,14 @@
 import BikMagic from '../../assets/img/magic.jpg'
 import BikNimbus from '../../assets/img/nimbus.jpg'
 import BikNebula from '../../assets/img/nebula.jpg'
+import BikNimbusHome from '../../assets/img/nimbus-home.jpg'
+import BikMagicHome from '../../assets/img/magic-home.jpg'
+import BikNebulaHome from '../../assets/img/nebula-home.jpg'
 
 interface BikListProps {
   id: number
   img: string
+  imgHome: string
   price: string
   name: string
   description: string
@@ -16,6 +20,7 @@ export const Biks = [
   {
     id: 1,
     img: BikNimbus,
+    imgHome: BikNimbusHome,
     price: 'R$ 4999',
     name: 'Nimbus Stark',
     description:
@@ -26,6 +31,7 @@ export const Biks = [
   {
     id: 2,
     img: BikMagic,
+    imgHome: BikMagicHome,
     price: 'R$ 2499',
     name: 'Magic Might',
     description:
@@ -36,6 +42,7 @@ export const Biks = [
   {
     id: 3,
     img: BikNebula,
+    imgHome: BikNebulaHome,
     price: 'R$ 3999',
     name: 'Nebula Cosmic',
     description:
