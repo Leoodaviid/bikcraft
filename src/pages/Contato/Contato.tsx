280463a01187a6cb3e5691f1c44d9a8d55b526b5
Leoodaviid <david_ax7@hotmail.com>
2023-03-03 00:52:38 -0300

    page contato + estilos

--- src/pages/Contato/Contato.tsx
+++ src/pages/Contato/Contato.tsx
@@ -1,7 +1,114 @@
-import React from 'react'
+import { Container, Form, Article } from './styles'
+import { Head } from '../../components/Head/Head'
+import { UseTitle } from '../../components/Title/useTitle'
+import { Input } from '../../components/Form/Input'
+import { TextArea } from '../../components/Form/TextArea'
+import { FormButton } from '../../components/Button/Button'
+import { BikSubTitle } from '../../components/BikTitle/BikSubTitle'
+import InstagramIcon from '../../assets/icons/instagram-p.svg'
+import FacebookIcon from '../../assets/icons/facebook-p.svg'
+import HorarioIcon from '../../assets/icons/horario.svg'
+import YouTubeIcon from '../../assets/icons/youtube-p.svg'
+import RjImg from '../../assets/img/rj.jpg'
+import SpImg from '../../assets/img/sp.jpg'
 
 const Contato = () => {
-  return <section>Contato</section>
+  return (
+    <>
+      <Head title='Contato' />
+      <UseTitle />
+      <Container>
+        <div className='contact container'>
+          <section className='contact-data' aria-label='Endereço'>
+            <h2>Loja Online</h2>
+            <div className='contact-address'>
+              <p>Rua Ali Perto, 42</p>
+              <p>Rio de Janeiro - RJ</p>
+              <p>Brasil - Terra - Via Láctea</p>
+            </div>
+            <address>
+              <a href='mailto:contato@bikcraft.com'>contato@bikcraft.com</a>
+              <a href='mailto:assistencia@bikcraft.com'>assistencia@bikcraft.com</a>
+              <a href='tel:+5585999999999'>55 85 99999-9999</a>
+            </address>
+            <div className='contact-social'>
+              <a href='/'>
+                <img src={InstagramIcon} alt='Instagram' />
+              </a>
+              <a href='/'>
+                <img src={FacebookIcon} alt='Facebook' />
+              </a>
+              <a href='/'>
+                <img src={YouTubeIcon} alt='YouTube' />
+              </a>
+            </div>
+          </section>
+          <section className='contact-form' aria-label='Formulário'>
+            <Form>
+              <Input label='Nome' type='text' name='nome' placeholder='Seu nome' />
+              <Input label='Telefone' type='text' name='telefone' placeholder='(85) 99999-9999' />
+              <Input
+                className='col-2'
+                label='Email'
+                type='email'
+                name='email'
+                placeholder='contato@email.com'
+              />
+              <TextArea
+                className='col-2'
+                name='mensagem'
+                rows={5}
+                placeholder=' O que você precisa?'
+              />
+              <FormButton className='col-2'>Enviar mensagem</FormButton>
+            </Form>
+          </section>
+        </div>
+      </Container>
+      <Article>
+        <BikSubTitle className='container'>Lojas Locais</BikSubTitle>
+        <div className='store container'>
+          <div className='store-item'>
+            <img
+              src={RjImg}
+              alt='mapa marcando o endereço em Rua Ali Perto, 42 - Rio de Janeiro - RJ'
+            />
+            <div className='store-content'>
+              <h3>Rio de Janeiro</h3>
+              <div className='store-data'>
+                <p>Rua Ali Perto, 42</p>
+                <p>Rio de Janeiro - RJ</p>
+              </div>
+              <div className='store-data'>
+                <a href='mailto:rj@bikcraft.com'>rj@bikcraft.com</a>
+                <a href='tel:+5585999999999'>55 85 99999-9999</a>
+              </div>
+              <p className='store-time'>
+                <img src={HorarioIcon} alt='' /> 08/18h de seg à dom
+              </p>
+            </div>
+          </div>
+          <div className='store-item'>
+            <img src={SpImg} alt='mapa marcando o endereço em Rua Ali Perto, 25 - São Paulo - SP' />
+            <div className='store-content'>
+              <h3>São Paulo</h3>
+              <div className='store-data'>
+                <p>Rua Ali Perto, 25</p>
+                <p>São Paulo - SP</p>
+              </div>
+              <div className='store-data'>
+                <a href='mailto:sp@bikcraft.com'>sp@bikcraft.com</a>
+                <a href='tel:+5585999999999'>55 85 99999-9999</a>
+              </div>
+              <p className='store-time'>
+                <img src={HorarioIcon} alt='' /> 08/18h de seg à dom
+              </p>
+            </div>
+          </div>
+        </div>
+      </Article>
+    </>
+  )
 }
 
 export default Contato
---
+++ src/pages/Contato/styles.ts
@@ -0,0 +1,164 @@
+import styled from 'styled-components'
+import IconLocal from '../../assets/icons/local.svg'
+import IconEmain from '../../assets/icons/email.svg'
+import IconPhone from '../../assets/icons/telefone.svg'
+
+export const Container = styled.div`
+  padding-bottom: 0.5rem;
+  background: linear-gradient(
+    ${({ theme }) => theme.colors.black} 280px,
+    ${({ theme }) => theme.colors.gray100} 0px
+  );
+  .contact {
+    display: grid;
+    grid-template-columns: 5fr 7fr;
+  }
+  .contact-data,
+  .contact-form {
+    padding: 3.75rem;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  }
+  .contact-data {
+    font: 400 ${({ theme }) => theme.fontSize.base} / 1.5 ${({ theme }) => theme.fontFamily.rob};
+    color: ${({ theme }) => theme.colors.gray500};
+    border-radius: 5px 0 0 5px;
+    background: ${({ theme }) => theme.colors.gray900};
+  }
+  .contact-form {
+    border-radius: 0 5px 5px 0;
+    background: ${({ theme }) => theme.colors.white};
+  }
+  .contact-data h2 {
+    font: 400 ${({ theme }) => theme.fontSize.xl} / 1.35 ${({ theme }) => theme.fontFamily.heading};
+    color: ${({ theme }) => theme.colors.white};
+    margin-bottom: 3.75rem;
+  }
+  .contact-address {
+    margin-bottom: 2rem;
+    padding-left: 1.75rem;
+    background: url(${IconLocal}) no-repeat 0px 2px;
+  }
+  .contact-address p {
+    margin-bottom: 0.5rem;
+  }
+  address a {
+    display: block;
+    max-width: max-content;
+    margin-bottom: 0.5rem;
+    padding-left: 1.75rem;
+  }
+  address a:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  address a:first-child {
+    background: url(${IconEmain}) no-repeat 0px 2px;
+  }
+  address a:nth-child(2) {
+    margin-bottom: 2rem;
+  }
+  address a:nth-child(3) {
+    background: url(${IconPhone}) no-repeat 0px 2px;
+    margin-bottom: 3.75rem;
+  }
+  .contact-social {
+    display: flex;
+    gap: 2rem;
+    padding-left: 1.75rem;
+  }
+  @media (max-width: 800px) {
+    .contact {
+      grid-template-columns: 1fr;
+    }
+    .contact-data,
+    .contact-form {
+      padding: 1.25rem;
+    }
+    .contact-data {
+      border-radius: 5px 5px 0 0;
+    }
+    .contact-form {
+      border-radius: 0 0 5px 5px;
+    }
+    .contact-data h2,
+    address a:nth-child(3) {
+      margin-bottom: 2rem;
+    }
+    .contact-social {
+      margin-bottom: 0.75rem;
+    }
+  }
+`
+
+export const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.25rem;
+  .col-2 {
+    grid-column: span 2;
+  }
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    .col-2 {
+      grid-column: 1;
+    }
+  }
+`
+
+export const Article = styled.article`
+  background-color: ${({ theme }) => theme.colors.gray100};
+  padding: 7.5rem 0 7.5rem;
+  h2 {
+    margin-bottom: 3.75rem;
+  }
+  .store {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0 2.5rem;
+  }
+  .store-item img {
+    border-radius: 0.25rem 0.25rem 0 0;
+  }
+  .store-item {
+    border-radius: 0.25rem;
+    background-color: ${({ theme }) => theme.colors.white};
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  }
+  .store-content {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem 1rem;
+    padding: 2rem;
+  }
+  .store-content h3,
+  .store-time {
+    grid-column: 1/-1;
+  }
+  .store-data {
+    display: grid;
+    gap: 0.5rem;
+    border-left: 2px solid ${({ theme }) => theme.colors.gray200};
+    padding-left: 0.75rem;
+  }
+  .store-time {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  @media (max-width: 800px) {
+    padding: 3.75rem 0 3.75rem 0;
+    h2 {
+      margin-bottom: 2rem;
+    }
+    .store {
+      gap: 1.25rem;
+    }
+    .store-content {
+      grid-template-columns: 1fr;
+    }
+  }
+  @media (max-width: 600px) {
+    .store {
+      grid-template-columns: 1fr;
+    }
+  }
+`
