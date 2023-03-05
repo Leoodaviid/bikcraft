2922e4cfbecc5dd40baec40c9e8ecadb97ac3bde
Leoodaviid <david_ax7@hotmail.com>
2023-03-05 14:51:42 -0300

    incluindo page Orçamento

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/Main'
 import Home from './pages/Home/Home'
 import Bicicletas from './pages/Bicicletas/Bicicletas'
+import Nimbus from './pages/Bicicletas/Nimbus'
+import Magic from './pages/Bicicletas/Magic'
+import Nebula from './pages/Bicicletas/Nebula'
 import Seguros from './pages/Seguros/Seguros'
 import Contato from './pages/Contato/Contato'
-import Magic from './pages/Bicicletas/Magic'
 import Termos from './pages/Termos/Termos'
-import Nimbus from './pages/Bicicletas/Nimbus'
-import Nebula from './pages/Bicicletas/Nebula'
+import Orcamento from './pages/Orcamento/Orcamento'
 
 export function AppRoutes() {
   return (
@@ -21,6 +22,7 @@ export function AppRoutes() {
         <Route path='seguros' element={<Seguros />} />
         <Route path='contato' element={<Contato />} />
         <Route path='termos' element={<Termos />} />
+        <Route path='orcamento' element={<Orcamento />} />
       </Route>
     </Routes>
   )
